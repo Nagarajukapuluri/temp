52a216337d3c021667a9628166cb7f4cd1fa0840
Nagaraju Kapuluri <kapulurinr@gmail.com>
2020-05-21 20:05:29 +0530

    array.c file modified

--- array.c
+++ array.c
@@ -1,10 +1,17 @@
 #include<stdio.h>
 void main()
 {
- int arr[7]={1,2,3,4,5,6.7};
- printf("The array elements are\t");
- for(int i=n;i>0;i--)
-  printf("%d",arr[i]);
+ int *p,n;
+ printf("Enter number of elements:\t");
+ scanf("%d",&n);
+ p=(int *)malloc(sizeof(int)*n);
+ printf("Enter elements to array\n");
+ for(int i=0;i<n;i++)
+   scanf("%d",p+i);
+ printf("Elements stored successfully\n array elements are\t");
+ for(int i=0;i<n;i++)
+   printf("%d\t",*(p+i));
+ printf("\n");
  return 0;
 }
  
